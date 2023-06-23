18ffa714a9dcac53a2306a32dfe98c11f7879181
kPherox <kphrx@kpherox.dev>
2023-06-23 22:48:41 +0900

    feat: add swift-format feature

---
+++ src/swift-foramt/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+eval "$(swift --version | awk '/Swift/{print $3}' | sed 's/\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)/export SWFIT_MAJOR=\1 SWFIT_MINOR=\2 SWFIT_PATCH=\3/')"
+
+if [ "$SWIFT_MAJOR" -eq 5 ] && [ "$SWIFT_MINOR" -ge 5 ]; then
+    SWIFT_FORMAT_BRANCH="swift-$SWIFT_MAJOR.$SWIFT_MINOR-branch"
+else
+    SWIFT_FORMAT_BRANCH="release/$SWIFT_MAJOR.$SWIFT_MINOR"
+fi
+
+cd /opt || exit
+git clone -b "$SWIFT_FORMAT_BRANCH" https://github.com/apple/swift-format.git
+cd swift-format || exit
+
+swift build -c release
+
+cp -f .build/release/swift-format /usr/local/bin/
+chmod +rx /usr/local/bin/swift-format
