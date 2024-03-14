9429679236563268b7b999088deb630fc42f437f
kPherox <kphrx@kpherox.dev>
2024-03-14 17:42:26 +0900

    fix: build swift-format failed for swift 5.6

--- features/swift-format/install.sh
+++ features/swift-format/install.sh
@@ -6,7 +6,7 @@ eval "$(swift --version | awk '/Swift/{print $3}' | sed 's/^\([0-9]\+\)\.\([0-9]
 export SWIFT_MINOR=\2/')"
 
 if [ "$SWIFT_FORMAT_BRANCH" = "auto" ]; then
-    if [ "$SWIFT_MAJOR" -eq 5 ] && [ "$SWIFT_MINOR" -lt 7 ]; then
+    if [ "$SWIFT_MAJOR" -eq 5 ] && [ "$SWIFT_MINOR" -lt 6 ]; then
         SWIFT_FORMAT_BRANCH="swift-$SWIFT_MAJOR.$SWIFT_MINOR-branch"
     else
         SWIFT_FORMAT_BRANCH="release/$SWIFT_MAJOR.$SWIFT_MINOR"
