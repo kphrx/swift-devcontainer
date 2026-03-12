7430e22f147f7e762a306cca83f7248186d61e39
kPherox <kphrx@kpherox.dev>
2026-03-12 19:17:45 +0900

    fix: memo patch file path

--- features/swift-format/install.sh
+++ features/swift-format/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SWIFT_FORMAT_BRANCH="${BRANCHNAME:-"auto"}"
+PATCH_FILE_0001="$(realpath "$(dirname $0)/0001-use-fork.patch")"
 
 eval "$(swift --version | awk '/Swift/{print $3}' | sed 's/^\([0-9]\+\)\.\([0-9]\+\)\(\.[0-9]\+\)\?\(-dev\)\?$/export SWIFT_MAJOR=\1\
 export SWIFT_MINOR=\2/')"
@@ -13,12 +14,11 @@ if [ "$SWIFT_FORMAT_BRANCH" = "auto" ]; then
     fi
 fi
 
-cd /opt || exit 1
-git clone -b "$SWIFT_FORMAT_BRANCH" https://github.com/swiftlang/swift-format.git || exit 1
-cd swift-format || exit 1
+git clone -b "$SWIFT_FORMAT_BRANCH" https://github.com/swiftlang/swift-format.git /opt/swift-format || exit 1
+cd /opt/swift-format || exit 1
 
 if [ "$SWIFT_MAJOR" -eq 5 ] && [ "$SWIFT_MINOR" -eq 8 ]; then
-    git apply --reject "$(dirname $0)/0001-use-fork.patch"
+    git apply --reject "$PATCH_FILE_0001"
 fi
 
 swift build -c release || exit 1
